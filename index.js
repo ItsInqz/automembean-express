62f06d223fe1682a75e5f680d304dada876aafb5
nateloeffel <imrekkrr@gmail.com>
2024-01-20 22:39:58 -0500

    added schema

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
+const cors = require('cors');
 
 const prisma = new PrismaClient();
 const app = express();
 
+app.use(cors()); // Enable CORS for all routes and origins
 app.use(express.json());
 
 // Example route
-app.get('/items', async (req, res) => {
-  const items = await prisma.item.findMany();
-  res.json(items);
-});
+app.get('/users', async (req, res) => {
+    const users = await prisma.user.findMany();
+    res.json(users)
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
